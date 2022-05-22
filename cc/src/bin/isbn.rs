4eacd4f38ddac771dc47b43ba537d0e51ca2e4a1
george velimachitis <3185416+gvelim@users.noreply.github.com>
2022-05-22 17:54:39 +0100

    commit

--- cc/src/bin/isbn.rs
+++ cc/src/bin/isbn.rs
@@ -1,10 +1,12 @@
 extern crate core;
 use std::str::FromStr;
 
+#[derive(Debug)]
 struct ISBN {
     isbn: String,
 }
 
+#[derive(Debug)]
 enum ISBN_Err {
     InputTooLong,
     InputTooSort,
@@ -12,7 +14,7 @@ enum ISBN_Err {
 }
 
 impl FromStr for ISBN {
-    type Err = ();
+    type Err = ISBN_Err;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let sum : u32 = s.splitn(5,'-')
@@ -23,13 +25,17 @@ impl FromStr for ISBN {
             .map(|(i,n)| if i % 2 == 1 { n*3 } else {n} )
             .sum();
 
-        println!("Sum: {}", 10-10-sum % 10);
-        Ok(ISBN { isbn: s.to_string() })
+        if (10-10-sum % 10) == 0 {
+            Ok(ISBN { isbn: s.to_string() })
+        } else {
+            Err(ISBN_Err::FailedChecksum)
+        }
     }
 }
 
 fn main() {
     let isbn = ISBN::from_str("978-3-16-148410-0");
+    println!("{:?}",isbn);
 }
 
 #[cfg(test)]
