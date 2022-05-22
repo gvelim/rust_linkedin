052151895975d2e8a7aea6815348f6640d54cd61
george velimachitis <3185416+gvelim@users.noreply.github.com>
2022-05-22 17:46:49 +0100

    commit

---
+++ cc/src/bin/isbn.rs
@@ -0,0 +1,52 @@
+extern crate core;
+use std::str::FromStr;
+
+struct ISBN {
+    isbn: String,
+}
+
+enum ISBN_Err {
+    InputTooLong,
+    InputTooSort,
+    FailedChecksum
+}
+
+impl FromStr for ISBN {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let numbers = String::new();
+        let sum : u32 = s.splitn(5,'-')
+            .fold(numbers,|c, i| c + i)
+            .chars()
+            .map(|s| if s.is_ascii_digit() { u32::from(s)-48 } else {panic!("non digit")} )
+            .enumerate()
+            .map(|(i,n)| if i % 2 == 1 { n*3 } else {n} )
+            .sum();
+
+        println!("Sum: {}", sum % 10);
+        Ok(ISBN { isbn: s.to_string() })
+    }
+}
+
+fn main() {
+    let isbn = ISBN::from_str("978-3-16-148410-0");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_input_sort() {
+        todo!()
+    }
+    #[test]
+    fn test_input_long() {
+        todo!()
+    }
+    #[test]
+    fn test_checksup() {
+        todo!()
+    }
+}
